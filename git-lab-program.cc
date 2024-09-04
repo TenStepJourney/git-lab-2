503604d81ef9bf457aad0aaf9331b84113ab6128
Eric Hanson <eric.julien.hanson@gmail.com>
2024-09-04 18:09:49 -0400

    Create git-lab-program.cc
    Adding git lab program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Eric Hanson
+ *        Date: 9/4/24
+ * Description: hello git?
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    return 0;
+}// main
